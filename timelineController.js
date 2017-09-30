f184256ef2a6f6cdacf167761b1cf8faceba91b7
Dennis <dsun96@gmail.com>
2017-09-30 09:57:33 -0400

    updates

--- timelineController.js
+++ timelineController.js
@@ -1,13 +1,20 @@
 angular.module('myApp.controllers', []).
 controller('timelineController',['$scope','$timeout', '$http', function($scope,$timeout, $http) {
   $scope.pull() = function() {
-      $http.get('/getTimeline', $scope..master).then(function(response) {
-             $scope.data = response.data;                                  
+      $http.get('/getTimeline', $scope.master).then( function(response) {
+             $scope.data = response.data;    
+                
                     },
+                                                    
         function(response) {
           console.log("no");
-      }
       
-  }
- };
-]);
+      });
+      
+      
+      var jsonObj = JSON.parse($scope.data);
+      alert("asdf");
+  };
+    
+    
+}]);
