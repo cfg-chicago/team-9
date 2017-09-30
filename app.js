07721abdeb50cc5d69d964d50946de24779d07c2
Justin V. Ledinh <Justin.Vinh.Ledinh@ibm.com>
2017-09-29 20:52:45 -0500

    added timeline

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 angular.module('myApp', [
-  'myApp.controllers'
+  'myApp.controllers',
+  'angular-timeline'
 ]);
--- eventformcontroller.js
+++ eventformcontroller.js
@@ -4,7 +4,18 @@ controller('eventformcontroller', function($scope) {
   $scope.eventtypes=['Field Trip','Weekly Update','End of Year Project','Family Dinner'];
 
   $scope.master={};
+  $scope.events = [];
   $scope.update = function(user) {
     $scope.master = angular.copy(user);
-  }
+    $scope.events.push({
+      badgeClass: 'info',
+      badgeIconClass: 'glyphicon-check',
+      eventname: $scope.master.eventname,
+      eventtype: $scope.master.eventtype,
+      date: $scope.master.date,
+      moment: $scope.master.moment,
+      learn: $scope.master.learn
+    })
+  };
+
 });
